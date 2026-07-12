417c259c3c950b7aa4ea1aa3d35aaea65088f39a
Maurya Suryakant <mauryasuryakant99@gmail.com>
2026-07-12 21:01:09 +0530

    Add initial HTML, CSS, and JavaScript files for project setup

---
+++ Project/one/script.js
@@ -0,0 +1,18 @@
+// const main = document.querySelector("main")
+const box = document.querySelector(".box")
+const btn = document.querySelector("button")
+
+btn.addEventListener("click", () => {
+    setTimeout(() => {
+        
+    })
+})
+
+
+
+
+// let rY = Math.floor(Math.random() * 80)
+// let rX = Math.floor(Math.random() * 80)
+
+// box.style.top = `${rY}%`;
+// box.style.left = `${rX}%`;
